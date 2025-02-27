47b7022719ff74dea3e05b914671dd21c79c0b03
Danny Armijos <daniel2667@hotmail.es>
2025-02-27 19:38:54 +0100

    Agregar manejo de errores y respuesta HTTP en la creación de Pokémon, incluyendo validación de duplicados y excepciones personalizadas.

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,12 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
-    return createPokemonDto;
+    try {
+      return await this.pokemonModel.create(createPokemonDto);
+    } catch (err: any) {
+      if (err.code === 11000) {
+        throw new BadRequestException(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          `Pokemon ${JSON.stringify(err.keyValue)} already exists`,
+        );
+      } else {
+        throw new InternalServerErrorException('Cannot create pokemon');
+      }
+    }
   }
 
   findAll() {
