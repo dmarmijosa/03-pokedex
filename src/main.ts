ace0f7552b0360c0d9c066f34dbc473a1a6dc05a
Danny Armijos <daniel2667@hotmail.es>
2025-02-27 18:48:16 +0100

    Agregar validación y transformación de datos en la creación de Pokémon, incluyendo DTO y configuración global de pipes.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //cambio de prefijo de la ruta de manera global
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //solo pasa los campos que estan en el dto
+      whitelist: true,
+      //evita que se envien campos que no estan en el dto
+      forbidNonWhitelisted: true,
+      //transforma los tipos de datos a los que se especifico en el dto
+      transform: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePokemonDto {}
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  IsInt,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+export class CreatePokemonDto {
+  @IsInt()
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
